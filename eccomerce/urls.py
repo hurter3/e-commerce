d43dbc224f5fffb05ca641143cb553d44cd0c314
hurter3 <mark.hurter@hotmail.co.uk>
2020-04-15 11:47:08 +0000

    corrected name of context to contexts and included it into the settings.py

--- eccomerce/urls.py
+++ eccomerce/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products
 from cart import urls as urls_cart
-from checkout import urls as urls_checkout
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT

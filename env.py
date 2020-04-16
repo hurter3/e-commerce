143eb1106a38c9b55eacfda36a4ab7f60c159a42
hurter3 <mark.hurter@hotmail.co.uk>
2020-04-16 09:03:08 +0000

    added icons to base

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault('STRIPE_PUBLISHABLE','pk_test_59CXWaba699b3rhLDSFXcnea00QqVbD9lX')
-os.environ.setdefault('STRIPE_SECRET','sk_test_wjGDKTVrcItAyPdZXgF5Q3L500AJMI4Pv3')
-os.environ.setdefault('SECRET_KEY','qzpw+d@jxe0#x-hf%26zoh^o=m^x3%@r(h64)qsst$byo$2u4w')
-os.environ.setdefault('DATABASE_URL','postgres://kregjxqbjrxyji:163ed15a075227f3acee336fc0283a8878b0c4caa438cd9e7152e4fd7b185983@ec2-54-247-103-43.eu-west-1.compute.amazonaws.com:5432/d44vg549qr5l6s')
-os.environ.setdefault('AWS_ACCESS_KEY_ID','AKIAVXV62REBP46CWDGY')
-os.environ.setdefault('AWS_SECRET_ACCESS_KEY','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')
